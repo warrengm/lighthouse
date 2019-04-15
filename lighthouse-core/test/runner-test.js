cfbf590f44636da4260c6ef974916d328781dcd2
Ward Peeters <ward@coding-tech.com>
2019-04-15 22:10:48 +0200

    fix test

--- lighthouse-core/test/runner-test.js
+++ lighthouse-core/test/runner-test.js
@@ -110,7 +110,7 @@ describe('Runner', () => {
     });
 
     it('-A throws if the URL changes', async () => {
-      const settings = {auditMode: artifactsPath};
+      const settings = {auditMode: artifactsPath, emulatedFormFactor: 'desktop'};
       const opts = {url: 'https://differenturl.com', config: generateConfig(settings), driverMock};
       try {
         await Runner.run(null, opts);
